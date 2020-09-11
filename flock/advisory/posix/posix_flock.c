01cf360475ec2b68888a3e4bf4ecc77629da56e6
qiuzerzhang <qiuzerzhang@tencent.com>
2020-09-11 16:39:01 +0800

    rename posix flock

--- flock/advisory/posix/posix_flock.c
+++ flock/advisory/posix/posix_flock.c
@@ -10,33 +10,35 @@
 #include "util.h"
 
 
-int wlock(struct flock *lock, int fd, int start, int len){
-    lock->l_type = F_WRLCK; //F_RDLCK
-    lock->l_whence = SEEK_SET;
-    lock->l_start = start;
-    lock->l_len = len;
-    lock->l_pid = 0; //getpid()
-    if (fcntl(fd, F_SETLKW, lock) == -1) {
+int wlock(struct flock *fl, int fd, int start, int len){
+    fl->l_type = F_WRLCK; //F_RDLCK
+    fl->l_whence = SEEK_SET;
+    fl->l_start = start;
+    fl->l_len = len;
+    fl->l_pid = 0; //getpid()
+    if(fcntl(fd, F_SETLKW, fl) == -1){
         return -1;
     }
     return 0;
 }
 
-int rlock(struct flock *lock, int fd, int start, int len){
-    lock->l_type = F_WRLCK; //F_RDLCK
-    lock->l_whence = SEEK_SET;
-    lock->l_start = start;
-    lock->l_len = len;
-    lock->l_pid = 0; //getpid();
-    if (fcntl(fd, F_SETLK, lock) == -1) {
+int rlock(struct flock *fl, int fd, int start, int len){
+    fl->l_type = F_WRLCK; //F_RDLCK
+    fl->l_whence = SEEK_SET;
+    fl->l_start = start;
+    fl->l_len = len;
+    fl->l_pid = 0; //getpid();
+    if(fcntl(fd, F_SETLK, fl) == -1){
+        perror("fcntl F_SETLK");
         return -1;
     }
     return 0;
 }
 
-int unlock(struct flock *lock, int fd){
-    lock->l_type = F_UNLCK;
-    if (fcntl(fd, F_SETLK, lock) == -1) {
+int unlock(struct flock *fl, int fd){
+    fl->l_type = F_UNLCK;
+    if(fcntl(fd, F_SETLK, fl) == -1){
+        perror("fcntl F_SETLK");
         return -1;
     }
     return 0;
