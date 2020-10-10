cb0fb99afe1ed39a5f943a94afe51b1d70932867
zhuichao001 <zhuichao001@163..com>
2020-10-10 17:12:23 +0800

    提交std::function

---
+++ std/std_function.cpp
@@ -0,0 +1,47 @@
+#include <functional>
+#include <iostream>
+
+using namespace std;
+
+std::function<int(int)> mathcall;
+
+int normal(int k) {
+    return -k;
+}
+
+auto lambda = [](int k)->int{ return k*k; };
+
+class functor {
+  public:
+    int operator()(int k) {
+        return k/2;
+    }
+};
+
+class counter {
+public:
+    int inc(int k) { return k+1; }
+    int dec(int k) { return k-1; }
+    static int digits(int k) { int n=0; while(k!=0){k/=10;++n;}; return n; }
+};
+
+int main() {
+    mathcall = normal;
+    cout << "normal:" << mathcall(1) << endl;
+
+    mathcall = lambda;
+    cout << "lambda:" << mathcall(2) << endl;
+
+    functor f;
+    mathcall = f;
+    cout << "functor:" << mathcall(3) << endl;
+
+    counter caller;
+    mathcall = std::bind(&counter::inc, caller, std::placeholders::_1);
+    cout << "class member func:" << mathcall(4) << endl;
+
+    mathcall = counter::digits;
+    cout << "class static func:" << mathcall(15) << endl;
+
+    return 0;
+}
