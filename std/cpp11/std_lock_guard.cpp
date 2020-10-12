470721f161c842ae6192f6672e1ae5bae3028219
zhuichao001 <zhuichao001@163..com>
2020-10-12 19:57:49 +0800

    add std_lock_guard case

---
+++ std/cpp11/std_lock_guard.cpp
@@ -0,0 +1,28 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+ 
+int g_count = 0;
+std::mutex g_mutex;
+ 
+void safe_increment() {
+    const std::lock_guard<std::mutex> lock(g_mutex);
+
+    for(int i=0;i<1000000;++i) g_count++;
+
+    std::cout << std::this_thread::get_id() << ": " << g_count << '\n';
+    // g_i_mutex is automatically released when lock goes out of scope
+}
+ 
+int main() {
+    std::cout << "at the beginning: " << g_count << '\n';
+ 
+    std::thread t1(safe_increment);
+    std::thread t2(safe_increment);
+ 
+    t1.join();
+    t2.join();
+ 
+    std::cout << "at the end: " << g_count << '\n';
+    return 0;
+}
