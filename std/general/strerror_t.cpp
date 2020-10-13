452baf9ac0cf58b8bae5084941c914d004f85130
zhuichao001 <zhuichao001@163..com>
2020-10-13 11:19:56 +0800

    modify strerror_t

--- std/general/strerror_t.cpp
+++ std/general/strerror_t.cpp
@@ -4,11 +4,11 @@
 
 std::string strErrno(const int errnum) {
     thread_local static char errbuf[256] = {'\0'};
-    char* ret = ::strerror_r(errnum, errbuf, 255);
-    return std::string(ret);
+    return std::string(::strerror_r(errnum, errbuf, 255));
 }
 
 int main(){
-    std::cout<<"[strerror]: "<<strErrno(23)<<std::endl;
+    std::cout<<"[strerror] of 23: "<<strErrno(23)<<std::endl;
+    std::cout<<"[strerror] of 28: "<<strErrno(28)<<std::endl;
     return 0;
 }
