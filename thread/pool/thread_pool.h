fc10dc2480399634793d50de9112ce4e9bcf33b0
zhuichao001 <zhuichao001@163.com>
2020-12-14 18:34:47 +0800

    fix bug

--- thread/pool/thread_pool.h
+++ thread/pool/thread_pool.h
@@ -18,11 +18,12 @@ public:
 
     thread_pool_t(size_t n):size(0),idle(0){
         scaleup(n);
+        running = true;
     }
 
     ~thread_pool_t(){
         running = false;
-        cv.notify_all(); 
+        cov.notify_all(); 
         for(std::thread &th : threads){
             if(th.joinable()){
                 th.join();
@@ -33,20 +34,19 @@ public:
     template<class F, class... A>
     decltype(auto) post(F&& func, A&&... args){
         if(!running){
-            throw runtime_error("post failed: thread_pool is not running.");
+            throw std::runtime_error("post failed: thread_pool is not running.");
         }
-        using return_type = typename std::result_of_t<F(Args...)>;
-        auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(forward<F>(func), std::forward<A>(args)...));
-        future<return_type> res = task->get_future();
+        using return_type = typename std::result_of_t<F(A...)>;
+        auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(func), std::forward<A>(args)...));
         if(true){
             std::lock_guard<std::mutex> locker{mtx};
-            tasks.emplace( [task](){(*task)();} );
+            tasks.emplace([task](){(*task)();});
         }
         if(idle<1){
             scaleup(1);
         }
-        cv.notify_one(); 
-        return res;
+        cov.notify_one(); 
+        return task->get_future();
     }
 
 private:
@@ -62,7 +62,7 @@ private:
                     task_t task;
                     if(true){
                         std::unique_lock<std::mutex> locker{mtx};    
-                        cv.wait(locker, [this]{return !this->running||!this-tasks.empty();});
+                        cov.wait(locker, [this]{return !this->running||!this-tasks.empty();});
                         if(!this->running && this->tasks.empty()){
                             return;
                         }
@@ -81,7 +81,7 @@ private:
     std::vector<std::thread> threads;
     std::queue<task_t> tasks;
     std::mutex mtx;
-    std::condition_variable cv;
+    std::condition_variable cov;
     std::atomic<bool> running;
     std::atomic<int> idle;
     std::atomic<int> size;
