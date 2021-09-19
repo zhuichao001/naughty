9adb88bbf262358705992cbb4fc63e70e873fec6
zhuichao001 <zhuichao001@163.com>
2021-09-19 08:25:17 +0800

    compare ops between std::map and std::unordered_map

--- std/general/test_map_ops.cpp
+++ std/general/test_map_ops.cpp
@@ -6,12 +6,13 @@
 #include "hlc/watch.h"
 
 watch_t t(Precision::MILISECOND);
+const int N = 1000*1000; //100w
 
 void test_map(){
     std::unordered_map<std::string,std::string> strmap;
 
     int64_t start = t.now();
-    for(int i=0; i<5000*1000; ++i){
+    for(int i=0; i<N; ++i){
         const std::string &key = std::to_string(i);
         const std::string &val = std::to_string(uint64_t(i)*i);
         strmap[key] = val;
@@ -24,7 +25,7 @@ void test_unordered_map(){
     std::map<std::string,std::string> strmap;
 
     int64_t start = t.now();
-    for(int i=0; i<5000*1000; ++i){
+    for(int i=0; i<N; ++i){
         const std::string &key = std::to_string(i);
         const std::string &val = std::to_string(uint64_t(i)*i);
         strmap[key] = val;
