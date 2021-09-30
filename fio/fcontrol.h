2476706714aef8c0d3d1d043ae90698258db925a
zhuichao001 <zhuichao001@163.com>
2021-10-01 06:03:40 +0800

    commti set_flag

---
+++ fio/fcontrol.h
@@ -0,0 +1,18 @@
+#include <fcntl.h>
+#include <stdio.h>
+
+int set_flag(int fd, int flags){
+    int val;
+    if((val=fcntl(fd, F_GETFL, 0)) < 0){
+        fprintf(stderr, "fcntl F_GETFL error\n");
+        return -1;
+    }
+
+    val |= flags;
+
+    if(fcntl(fd, F_SETFL, val) < 0){
+        fprintf(stderr, "fcntl F_SETFL error\n");
+        return -1;
+    }
+    return 0;
+}
