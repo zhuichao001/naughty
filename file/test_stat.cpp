d50d72c85f54553715f9ea89377c2654bafdeb06
zhuichao001 <zhuichao001@163.com>
2021-10-01 06:39:52 +0800

    commit stat test

---
+++ file/test_stat.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <sys/stat.h>
+
+int test_stat(){
+    struct stat st;
+    if(lstat("./test_stat.cpp", &st)<0){
+        fprintf(stderr, "lstat(2) error\n");
+        return -1;
+    }
+
+    const char *type;
+
+    if(S_ISREG(st.st_mode)){
+        type = "regular";
+    }else if(S_ISDIR(st.st_mode)){
+        type = "directory";
+    }else if(S_ISCHR(st.st_mode)){
+        type = "character special";
+    }else if(S_ISBLK(st.st_mode)){
+        type = "block special";
+    }else if(S_ISFIFO(st.st_mode)){
+        type = "fifo";
+    }else if(S_ISLNK(st.st_mode)){
+        type = "symbolic link";
+    }else if(S_ISSOCK(st.st_mode)){
+        type = "socket";
+    }else{
+        type = "[UNKNOWN MODE]";
+    }
+
+    printf("type:%s\n", type);
+    return 0;
+}
+
+int main(){
+    test_stat();
+}
