e20b363997b22a4c04b16bbf12d3a21b2bd63e4a
zhuichao001 <zhuichao001@163.com>
2021-10-01 06:10:31 +0800

    add clear flag function

--- fio/fcontrol.h
+++ fio/fcontrol.h
@@ -16,3 +16,19 @@ int set_flag(int fd, int flags){
     }
     return 0;
 }
+
+int clear_flag(int fd, int flags){
+    int val;
+    if((val=fcntl(fd, F_GETFL, 0)) < 0){
+        fprintf(stderr, "fcntl F_GETFL error\n");
+        return -1;
+    }
+
+    val &= ~flags;
+
+    if(fcntl(fd, F_SETFL, val) < 0){
+        fprintf(stderr, "fcntl F_SETFL error\n");
+        return -1;
+    }
+    return 0;
+}
