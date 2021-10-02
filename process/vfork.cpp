334190513add7717314b76f386c21275818644f5
zhuichao001 <zhuichao001@163.com>
2021-10-03 07:16:07 +0800

    modify

--- process/vfork.cpp
+++ process/vfork.cpp
@@ -1,21 +1,22 @@
 #include <unistd.h>  
 #include <stdio.h>  
 
+int gval = 9;
+
 int main(void) {  
-    pid_t pid;  
-    int val=0;
-    pid=vfork();  
+    int val=100;
+    pid_t pid=vfork();  
     if(pid<0){
         fprintf(stderr, "vfork error\n");
         return 0;
     } else if(pid==0) {
-        val++;
-        fprintf(stderr, "child, val:%d\n", pid, val);  
+        ++val;
+        ++gval;
+        fprintf(stderr, "child, val:%d, global val:%d\n", val, gval);  
         sleep(3);
-        _exit(0);
+        _exit(0); //will dead-clock if missed exit() or exec()
     } else {
-        val++;
-        fprintf(stderr, "parent, val:%d\n", pid, val);
+        fprintf(stderr, "parent, val:%d, global val:%d\n", val, gval);
     }
     return 0;
 }
