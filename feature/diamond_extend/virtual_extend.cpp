f2eb4dad4d84417db31ec8d9167ff7e441487b08
zhuichao001 <zhuichao001@163.com>
2022-02-08 18:33:20 +0800

    add virtual extend for diamond extend to solve consistency model

---
+++ feature/diamond_extend/virtual_extend.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+protected:
+    int value;
+public:
+    Base()=default;
+    Base(int v):
+        value(v){
+        std::cout<< "Base Called" <<std::endl;
+    }
+};
+
+class Father:virtual public Base{
+public:
+    Father()=default;
+    Father(int v):
+        Base(v){
+        std::cout<< "Father Called" <<std::endl;
+    }
+
+    void set_value(int value){
+        this->value=value;
+    }
+}; 
+
+class Mother:virtual public Base{
+public:
+    Mother()=default;
+    Mother(int v):
+        Base(v){
+        std::cout<< "Mother Called" <<std::endl;
+    }
+    int get_value(){
+        return this->value;
+    }
+};
+
+class Son:public Father,public Mother{
+public:
+    Son()=default;
+    Son(int v):
+        Father(v),
+        Mother(v),
+        Base(v){ //must actively call
+        std::cout<< "Son Called" <<std::endl;
+    }
+};
+
+int main(){
+    Son s(10);
+    s.set_value(20);
+    cout<<s.get_value()<<endl;
+    return 0;
+}
