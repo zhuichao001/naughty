717415f5cd8117679834a5ddeb5829253e04f054
zhuichao001 <zhuichao001@163.com>
2022-06-22 15:12:34 +0800

    add experiment for std::move

--- std/cpp1x/std_move.cpp
+++ std/cpp1x/std_move.cpp
@@ -17,8 +17,35 @@ void std_move_case() {
     std::cout << "The contents of the vector are \"" << v[0] << "\", \"" << v[1] << "\"\n";
 }
 
+//how to understand
+void std_move_is(){
+    {
+        int &&ref_a = 5;
+        ref_a = 6; 
+    }
+     
+    //A.K.A
+    {
+        int temp = 5;
+        int &&ref_a = std::move(temp);
+        ref_a = 6;
+    }
+}
 
-int main(){
+void std_move_self(){
+    auto change = [](int& left_value) {
+        left_value = 8;
+    };
+
+    int a = 5;
+    //a_right self is left-value, although std::move(a) is right-value
+    int &&a_right = std::move(a); 
+    change(a_right);
+    fprintf(stderr, "a:%d\n", a);
+}
+
+int main() {
+
     std_move_case();
     return 0;
 }
