543c40ab00d8171397f82d935f5e51eb211e039c
zhuichao001 <zhuichao001@163.com>
2022-07-17 12:49:21 +0800

    lock-free stack

---
+++ thread_safe/lock_free_stack.h
@@ -0,0 +1,36 @@
+//lock-free but not wait-free
+//memory leak!!!
+
+template <typename T>
+class lock_free_stack {
+private:
+    struct node {
+        std::shared_ptr<T> data;
+        node *next;
+        node(const T &v):
+            data(std::make_shared<T>(v)),
+            next(nullptr){
+        }
+    };
+
+    std::atomic<node*> head;
+public:
+    lock_free_stack():
+        head(nullptr){
+    }
+
+    void push(const T &t){
+        node *neo = new node(t);
+        neo->next = head.load();
+
+        while(head.compare_exchange_weak(neo->next, neo){ } 
+    }
+
+    std::_shared_ptr<T> pop(){
+        node *first = head.load();
+        while(first && !head.compare_exchange_weak(first, first->next)){ }
+
+        return fist ? first->data : std::shared_ptr<T>();
+    }
+
+};
