2a7a1bce2beb16561a59ab1e8b007189ffd068aa
zhuichao001 <zhuichao001@163.com>
2022-07-18 16:30:26 +0800

    use memory order to accelerate

--- thread_safe/lock_free_stack_v2.h
+++ thread_safe/lock_free_stack_v2.h
@@ -29,7 +29,7 @@ private:
         do{
             new_counter = old_counter;
             ++new_counter.external_count;
-        }while(!head.compare_exchange_strong(old_counter, new_counter));
+        }while(!head.compare_exchange_strong(old_counter, new_counter, std::memory_order_acquire, std::memory_order_relaxed));
         old_counter.external_count = new_counter.external_count;
     }
 public:
@@ -41,28 +41,29 @@ public:
         snode_counter_t neo;
         neo.ptr = new snode_t(data);
         neo.external_count = 1;
-        neo.ptr->next = head.load();
-        while(!head.compare_exchange_weak(neo.ptr->next, neo));
+        neo.ptr->next = head.load(std::memory_order_relaxed);
+        while(!head.compare_exchange_weak(neo.ptr->next, neo, std::memory_order_release, std::memory_order_relaxed));
     }
 
     std::shared_ptr<T> pop() {
-        snode_counter_t old_head = head.load();
+        snode_counter_t old_head = head.load(std::memory_order_relaxed);
         for(;;) {
             increase_head_count(old_head);
             snode_t* const ptr = old_head.ptr;
             if(!ptr) {
                 return std::shared_ptr<T>();
             }
-            if(head.compare_exchange_strong(old_head, ptr->next)) {
+            if(head.compare_exchange_strong(old_head, ptr->next, std::memory_order_relaxed)) {
                 std::shared_ptr<T> res;
                 res.swap(ptr->data);
 
                 const int count_increase = old_head.external_count-2;
-                if(ptr->internal_count.fetch_add(count_increase) == -count_increase) {
+                if(ptr->internal_count.fetch_add(count_increase, std::memory_order_release) == -count_increase) {
                     delete ptr;
                 }
                 return res;
-            } else if(ptr->internal_count.fetch_sub(1)==1) {
+            } else if(ptr->internal_count.fetch_sub(1, std::memory_order_relaxed)==1) {
+                ptr->internal_count.load(std::memory_order_acquire);
                 delete ptr;
             }
         }
