2022c78679b381ea19aff8fe6c2760d8827eae8d
zhuichao001 <zhuichao001@163.com>
2022-07-22 15:46:22 +0800

    rename

--- brk/main.c
+++ brk/main.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-char *fmalloc(int n) {
+char *Malloc(int n) {
 	char *mem = (char *)malloc(n);
 	if (mem == NULL) {
 		perror("malloc");
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	return mem;
 }
@@ -17,21 +17,21 @@ int main(void) {
 		printf ("heap start: %p\n", hstart);
 	}
 
-	char *pmem = fmalloc(64);
+	char *pmem = Malloc(64);
 	hstart = sbrk(0);
 	if (hstart != (void *)-1) {
 		printf ("pmem:%p, heap end:%d, heap size on each load: %p\n", pmem, hstart, (char *)hstart-pmem);
 	}
 	free(pmem);
 
-	char *pmem2 = fmalloc(128);
+	char *pmem2 = Malloc(128);
 	hstart = sbrk(0);
 	if (hstart != (void *)-1) {
 		printf ("pmem2:%p, heap end:%d, heap size on each load: %p\n", pmem2, hstart, (char *)hstart-pmem2);
 	}
 	free(pmem2);
 
-	char *pmem3 = fmalloc(132*1024); //mmap asign space
+	char *pmem3 = Malloc(132*1024); //mmap asign space
 	hstart = sbrk(0);
 	if (hstart != (void *)-1) {
 		printf ("pmem3:%p, heap end:%d, heap size on each load: %p\n", pmem3, hstart, (char *)hstart-pmem3);
