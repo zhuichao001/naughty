e48143703117cf214a4eec96803b931d23eeb7cc
zhuichao001 <zhuichao001@163.com>
2022-09-01 18:34:36 +0800

    modify CACHE_LINE_ALIGN

--- aligned/alignas.c
+++ aligned/alignas.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#ifndef CACHE_LINE_ALIGN
+#define CACHE_LINE_SIZE 64
+#define CACHE_LINE_ALIGN __attribute__((aligned(CACHE_LINE_SIZE)))
+#endif
+
+//aligned only supported by c++11 above
 struct alignas(64) node_t{
     int *left;
     int *right;
