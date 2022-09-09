51578bf95d1e0723c649dde1551409c274bae349
zhuichao001 <zhuichao001@163.com>
2022-09-09 19:05:33 +0800

    update Makefile

--- std/algorithm/std_heap.cpp
+++ std/algorithm/std_heap.cpp
@@ -12,11 +12,11 @@ bool cmp_pair(const pair<int,int> &a, const pair<int,int> &b){
 }
 
 void print_heap(vector<pair<int,int>> &v){
-    cout << "start ----" << endl;
+    cout << "start print----" << endl;
     for (auto e : v) {
         cout << e.first << ':' << e.second << endl;
     }
-    cout << "end ----" << endl;
+    cout << "end print----" << endl;
 }
 
 int main(){
@@ -26,7 +26,7 @@ int main(){
     v.push_back(make_pair(6,2));
     v.push_back(make_pair(12,10));
     make_heap(v.begin(), v.end(), cmp_pair);
-    //print_heap(v); //TODO
+
     while(!v.empty()){
         pop_heap(v.begin(), v.end(), cmp_pair);
         pair<int, int> cur = v.back();
@@ -34,7 +34,6 @@ int main(){
         int point = cur.first;
         int cost = cur.second;
         cout<< "pop point:" << point << ",cost:" << cost << endl;
-        //print_heap(v); //TODO
     }
 
     return 0;
