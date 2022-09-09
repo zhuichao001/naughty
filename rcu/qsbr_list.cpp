c56c2d3dadfc290a1d84c5601d7210741e530c41
zhuichao001 <zhuichao001@163.com>
2022-09-09 17:56:51 +0800

    complete first urcu example of list

---
+++ rcu/qsbr_list.cpp
@@ -0,0 +1,119 @@
+#include <urcu-qsbr.h>
+#include <pthread.h>
+#include <poll.h>
+
+struct node{
+    int key;
+    int val;
+    struct node *next;
+
+    node(int k=0, int v=0, struct node *ptr=nullptr):
+        key(k),
+        val(v),
+        next(ptr){
+    }
+} head;
+
+int list_query(const int k, int &v){
+    int err = -1;
+    v = -1;
+    rcu_read_lock();
+    struct node *cur = head.next;
+    while(cur!=nullptr){
+        if(cur->key==k){
+            v = cur->val;
+            err = 0;
+            break;
+        }
+    }
+    rcu_read_unlock();
+    return err;
+}
+
+int list_modify(const int k, const int v){
+    int err = -1;
+    rcu_read_lock();
+
+    struct node *neo = new struct node(k,v,nullptr);
+
+    struct node *cur = &head;
+    struct node *old = nullptr;
+    while(cur->next!=nullptr){
+        if(cur->next->key==k){
+            neo->next = cur->next->next;
+            old = cur->next;
+            err = 0;
+            break;
+        }
+    }
+    rcu_read_unlock();
+
+    if(err==0){
+        synchronize_rcu();
+        cur->next = neo;
+        delete old;
+    }
+    return err;
+}
+
+void *run_list_query(void *arg){
+    rcu_thread_online();
+
+    for(int i=0; i<1000000; ++i){
+        int k=i;
+        int v;
+        list_query(i, v);
+        if(i%100==0){
+            rcu_quiescent_state();
+        }
+    }
+
+    rcu_thread_offline();
+    return nullptr;
+}
+
+void *run_list_modify(void *arg){
+    rcu_thread_online();
+
+    for(int i=0; i<10000; ++i){
+        int k=i*100;
+        int v=i*1000;
+        list_modify(k, v);
+        poll(nullptr, 0, 100);
+    }
+
+    rcu_thread_offline();
+    return nullptr;
+}
+
+void init_list(){
+    struct node *tail = &head;
+    for(int i=0; i<1000000; ++i){
+        tail->next = new struct node(i, i, nullptr);
+        tail = tail->next;
+    }
+}
+
+int main(){
+    init_list();
+
+    const int N_READER = 4;
+    const int N_UPDATER = 2;
+    pthread_t pid_reader[N_READER];
+    pthread_t pid_updater[N_UPDATER];
+
+    for(int i=0; i<N_READER; ++i){
+        pthread_create(&pid_reader[i], NULL, run_list_query, NULL);
+    }
+    for(int i=0; i<N_UPDATER; ++i){
+        pthread_create(&pid_updater[i], NULL, run_list_modify, NULL);
+    }
+
+    for(int i=0; i<N_READER; ++i){
+        pthread_join(pid_reader[i], NULL);
+    }
+    for(int i=0; i<N_UPDATER; ++i){
+        pthread_join(pid_updater[i], NULL);
+    }
+    return 0;
+}
