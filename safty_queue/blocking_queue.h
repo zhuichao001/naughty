7f9d67f54bb0b553624b3035269360e2859b833a
zhuichao001 <zhuichao001@163.com>
2023-03-01 16:27:16 +0800

    modify

--- safty_queue/blocking_queue.h
+++ safty_queue/blocking_queue.h
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <deque>
 #include <mutex>
 #include <condition_variable>
@@ -28,17 +27,11 @@ public:
     }
 
     T pop() {
-        //std::cout << "to pop.1" << std::endl;
         std::unique_lock<std::mutex> lock(mutex_);
-        //std::cout << "to pop.2" << std::endl;
         convar_have_.wait(lock, [=]{return !queue_.empty();});
-        //std::cout << "to pop.3" << std::endl;
         T t(std::move(queue_.back()));
-        //std::cout << "to pop.4" << std::endl;
         queue_.pop_back();
-        //std::cout << "to pop.5" << std::endl;
         convar_rest_.notify_one();
-        //std::cout << "to pop.6" << std::endl;
         return t;
     }
 };
