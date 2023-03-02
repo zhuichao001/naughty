9876fabd72548377ae42670832e755bc3887ef99
zhuichao001 <zhuichao001@163.com>
2023-03-02 20:25:26 +0800

    modify

--- safty_queue/blocking_queue.h
+++ safty_queue/blocking_queue.h
@@ -10,8 +10,8 @@ private:
     const int capacity_;
     std::deque<T>   queue_;
     std::mutex      mutex_;
-    std::condition_variable convar_rest_;
-    std::condition_variable convar_have_;
+    std::condition_variable cv_notfull_;
+    std::condition_variable cv_notempty_;
 
 public:
     BlockingQueue(int n):
@@ -21,18 +21,18 @@ public:
     void push(T const& t) {
         {
             std::unique_lock<std::mutex> lock(mutex_);
-            convar_rest_.wait(lock, [=]{return queue_.size()!=capacity_;});
+            cv_notfull_.wait(lock, [=]{return queue_.size()!=capacity_;});
             queue_.push_front(t);
         }
-        convar_have_.notify_one();
+        cv_notempty_.notify_one();
     }
 
     T pop() {
         std::unique_lock<std::mutex> lock(mutex_);
-        convar_have_.wait(lock, [=]{return !queue_.empty();});
+        cv_notempty_.wait(lock, [=]{return !queue_.empty();});
         T t(std::move(queue_.back()));
         queue_.pop_back();
-        convar_rest_.notify_one();
+        cv_notfull_.notify_one();
         return t;
     }
 };
