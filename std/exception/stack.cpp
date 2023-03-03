964ed77b850033b1a901eaa819359efd90f09390
zhuichao001 <zhuichao001@163.com>
2023-03-03 16:03:57 +0800

    finish out_of_range except

---
+++ std/exception/stack.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <stdexcept>
+
+template <typename T>
+class Stack{
+    std::vector<T> elements;
+public:
+    void push(T const &t){
+        elements.push_back(t);
+    }
+
+    T pop(){
+        if(elements.size()==0){
+            throw std::out_of_range("Stack can not pop when empty.");
+        }
+
+        T e = elements.back();
+        elements.pop_back();
+        return e; 
+    }
+};
+
+int main(){
+    Stack<int> stack;
+    //stack.push(6);
+    stack.pop();
+}
