7d41769d99c7abf0eb4ed1a4be3b87764b12ffe4
zhuichao001 <zhuichao001@163.com>
2023-03-06 15:39:04 +0800

    call default constructors of members

---
+++ cpp_obj_model/default_constructor_v2.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+class Desk {
+public:
+    Desk(){
+        printf("default Desk construct called.\n");
+    }
+};
+
+class Picture {
+public:
+    int pid;
+};
+
+class Chair {
+public:
+    Chair(){
+        printf("default Chair construct called.\n");
+    }
+};
+
+class Room{
+    Desk desk;
+    Picture pic;
+    Chair chair;
+    char *ptr;
+public:
+    Room(){
+        //desk::Desk();
+        //no Picture default will be called
+        //chair::Chair();
+        ptr = nullptr;
+    }
+};
+
+int main(){
+    Room room;
+    return 0;
+}
