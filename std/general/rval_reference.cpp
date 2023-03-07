5458e9504a34d6e07ecf4ec984a795eb94689291
zhuichao001 <zhuichao001@163.com>
2023-03-07 10:44:02 +0800

    add right val reference

---
+++ std/general/rval_reference.cpp
@@ -0,0 +1,36 @@
+#include <utility>
+#include <stdio.h>
+
+class B{
+public:
+    B(){}
+
+    B(const B &b){
+        printf("copy construct called.\n");
+    }
+
+    B(B &&b){
+        printf("move construct called.\n");
+    }
+
+    B & operator=(const B & b){
+        printf("copy assignment called.\n");
+        return *this;
+    }
+
+    B & operator=(B && b){
+        printf("move assignment called.\n");
+        return *this;
+    }
+};
+
+void foo(B && b){
+    B first = b;
+    printf("==========\n");
+    B second = std::move(b);
+}
+
+int main(){
+    foo(B());
+    return 0;
+}
