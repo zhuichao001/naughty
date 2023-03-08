e76c01279f280ce34b6338b536f619e5a79c738b
zhuichao001 <zhuichao001@163.com>
2023-03-08 11:13:05 +0800

    modify overload

--- cpp_feature/overload.cpp
+++ cpp_feature/overload.cpp
@@ -7,10 +7,13 @@ class Entity{
 
 class Hub{
 public:
-    //int push(Entity en){printf("push(Entity)\n");return 0;}
+    //int push(Entity en){printf("push(Entity)\n");return 0;} /*给定类型和对该类型的引用将视为相同*/
     int push(Entity &en){printf("push(Entity&)\n");return 0;} /*the same to above*/
     
     //int push(const Entity e){return 0;} /*cannot be overloaded*/
+
+    /*对于重载而言，由 const 或 volatile 修饰的类型的函数参数的处理方式与基类型没有什么不同。
+     * 但是，函数重载机制可以区分由 const 和 volatile 限定的引用和对基类型的引用。*/
     int push(const Entity &en){printf("push(const Entity&)\n");return 0;}
 
     int push(Entity *en){printf("push(Entity*)\n");return 0;}
