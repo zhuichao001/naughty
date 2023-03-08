3e248e6e75a6374a862193d6acf51e8cbe39a388
zhuichao001 <zhuichao001@163.com>
2023-03-08 14:24:15 +0800

    add lambda

--- cpp_feature/lambda.cpp
+++ cpp_feature/lambda.cpp
@@ -1,3 +1,15 @@
+#include <stdio.h>
 
 //[ capture list ] ( parameter list) -> return type { function body }
 
+
+void test(){
+    auto plus = [] (int v1, int v2) -> int { return v1 + v2; };
+    int sum = plus(1, 2);
+    printf("sum:%d\n", sum);
+}
+
+int main(){
+    test();
+    return 0;
+}
