f701c600a445fabb15954a05c7e83fe9d984765a
zhuichao001 <zhuichao001@163.com>
2023-03-08 11:00:27 +0800

    perfect overload

--- cpp_feature/overload.cpp
+++ cpp_feature/overload.cpp
@@ -7,18 +7,31 @@ class Entity{
 
 class Hub{
 public:
-    int push(Entity e){return 0;}
-    int push(Entity &e){return 0;}
+    //int push(Entity en){printf("push(Entity)\n");return 0;}
+    int push(Entity &en){printf("push(Entity&)\n");return 0;} /*the same to above*/
+    
     //int push(const Entity e){return 0;} /*cannot be overloaded*/
-    int push(const Entity &e){return 0;}
+    int push(const Entity &en){printf("push(const Entity&)\n");return 0;}
 
-    int find(Entity *e){return 0;}
-    int find(const Entity *e){return 0;}
-    //int find(Entity * const e){return 0;} /*cannot be overloaded*/
+    int push(Entity *en){printf("push(Entity*)\n");return 0;}
+    //int push(Entity * const en){printf("push(Entity*const)\n");return 0;} /*cannot be overloaded*/
+    int push(const Entity *en){printf("push(const Entity*)\n");return 0;}
+    //int push(Entity * const e){return 0;} /*cannot be overloaded*/
 
 };
 
 int main(){
     Hub h;
+    Entity e;
+    h.push(e);
+
+    const Entity &f = e;
+    h.push(f);
+
+    Entity *p = &e;
+    h.push(p);
+
+    const Entity *q = &f;
+    h.push(q);
     return 0;
 }
