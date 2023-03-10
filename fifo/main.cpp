00f4d33cd0190af7645dd42888e7233c30482a36
zhuichao001 <zhuichao001@163.com>
2023-03-11 05:44:25 +0800

    modify

--- fifo/main.cpp
+++ fifo/main.cpp
@@ -15,13 +15,15 @@ void write(){
     src[N]=0;
 
     for(int pos=0; pos<N; ){
-        int len = u.push((uint8_t*)src+pos, 26<N-pos);
+        int len = u.push((uint8_t*)src+pos, N-pos);
         if(len==0)continue;
 
-        char backup = src[pos+len];
-        src[pos+len] = 0;
-        //printf("write len=%d, data=%s\n", len, src+pos);
-        src[pos+len] = backup;
+        { //print what's pushed
+            char backup = src[pos+len];
+            src[pos+len] = 0;
+            fprintf(stderr, "write len=%d, data=%s\n", len, src+pos);
+            src[pos+len] = backup;
+        }
 
         pos+=len;
     }
@@ -30,11 +32,11 @@ void write(){
 void read(){
     char dst[N+1];
     for(int pos=0; pos<N; ){
-        int len = u.pop((uint8_t*)dst+pos, 26);
+        int len = u.pop((uint8_t*)dst+pos, 26); //read 26 at max
         if(len==0)continue;
         dst[pos+len] = 0;
 
-        //printf("read len=%d, data=%s\n", len, dst+pos);
+        fprintf(stderr, "read len=%d, data=%s\n", len, dst+pos);
 
         pos+=len;
     }
--- fifo/ufifo.h
+++ fifo/ufifo.h
@@ -16,7 +16,7 @@
 #define smp_wmb() COMPILER_BARRIER()  
 #endif
 
-#define min(x,y) ((x)<(y)?(x):(y))
+#define Min(x,y) ((x)<(y)?(x):(y))
 
 uint32_t roundup_pow_of_two(uint32_t v) {
     --v;
@@ -28,34 +28,20 @@ uint32_t roundup_pow_of_two(uint32_t v) {
 
 
 class ufifo{
+    uint32_t size; //2的整幂对齐
+    uint8_t *data;
+    uint32_t ridx;
+    uint32_t widx;
 public:
-    ufifo(uint8_t *buff, uint32_t buff_size){
-        size = roundup_pow_of_two(buff_size);
-        if(size!=buff_size){
-            buff = (uint8_t *)realloc(buff, size);
-        }
-
-        ridx = 0;
-        widx = 0;
-    }
-
-
     ufifo(uint32_t capacity){
         size = roundup_pow_of_two(capacity);
         data = (uint8_t *)malloc(size);
-        assert(data!=0);
-        alloced = true;
+        assert(data!=nullptr);
 
         printf("align %d is size:%d\n", capacity, size);
 
         ridx = 0;
         widx = 0;
-    }
-
-    void init(){
-        for(int i=0; i<size; ++i){
-            data[i] = 'a'+i%26;
-        }
     }
 
     uint32_t used()const{
@@ -68,9 +54,9 @@ public:
 
     uint32_t push(const uint8_t *src, uint32_t limit){
         uint32_t _unused = unused(); 
-        uint32_t len = min(limit, _unused);
+        uint32_t len = Min(limit, _unused);
         smp_mb();
-        uint32_t right = min(widx+len, size);
+        uint32_t right = Min(widx+len, size);
         uint32_t first = right-widx;
         memcpy(data+widx, src, first);
         memcpy(data, src+first, len-first);
@@ -81,8 +67,8 @@ public:
 
     uint32_t pop(uint8_t *dst, uint32_t limit){
         uint32_t _used = used();
-        uint32_t len = min(limit, _used);
-        uint32_t right = min(ridx+len, size);
+        uint32_t len = Min(limit, _used);
+        uint32_t right = Min(ridx+len, size);
         uint32_t first = right-ridx;
         smp_rmb();
         memcpy(dst, data+ridx, first);
@@ -93,17 +79,11 @@ public:
     }
 
     ~ufifo(){
-        if(alloced) {
+        if(data!=nullptr){
             free(data);
+            data = nullptr;
         }
     }
-
-private:
-    uint32_t size; //2的整幂对齐
-    uint8_t *data;
-    bool alloced;
-    uint32_t ridx;
-    uint32_t widx;
 };
 
 
