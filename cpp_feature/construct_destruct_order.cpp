7ede5cce9b5f33a1194bfd16a9dd25d323959d22
zhuichao001 <zhuichao001@163.com>
2023-03-13 06:01:55 +0800

    modify

--- cpp_feature/construct_destruct_order.cpp
+++ cpp_feature/construct_destruct_order.cpp
@@ -22,7 +22,8 @@ void func() {
     CDemo LocalObjectInFunc("LocalObjectInFunc");
     static CDemo StaticObject("StaticObject");
     CDemo* pHeapObjectInFunc = new CDemo("HeapObjectInFunc");
-    std::cout << "Inside func" << std::endl;
+    delete pHeapObjectInFunc;
+    std::cout << "before return from Inside func" << std::endl;
 }
 
 CDemo GlobalObject("GlobalObject");
@@ -31,9 +32,11 @@ int main() {
     CDemo LocalObjectInMain("LocalObjectInMain");
     CDemo* pHeapObjectInMain = new CDemo("HeapObjectInMain");
 
-    std::cout << "In main, before calling func\n";
+    std::cout << "In main, before calling func\n\n";
     func();
-    std::cout << "In main, after calling func\n";
+    std::cout << "\nIn main, again calling func\n\n";
+    func();
+    std::cout << "\nIn main, after calling func\n";
     delete pHeapObjectInMain;
     return 0;
 }
