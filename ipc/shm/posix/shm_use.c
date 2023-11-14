f3be330d1625b0e233dcf4bc151dc5f122f9a4e5
zhuichao001 <zhuichao001@163.com>
2023-11-14 23:45:46 +0800

    mod...

--- ipc/shm/posix/shm_use.c
+++ ipc/shm/posix/shm_use.c
@@ -7,15 +7,15 @@
 #include <fcntl.h>
 
 #define FILE_MODE 0X766
+#define MAP_SIZE 65536*4096
 
 int main(int argc, char **argv){
     int flags = O_RDWR | O_CREAT;
-    int length = 1024;
     const char *path = "tmp.shm_create";
     int fd = shm_open(path, flags, FILE_MODE); 
-    ftruncate(length, length);
+    ftruncate(fd, MAP_SIZE);
 
-    char *ptr = (char*)mmap(NULL, length, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    char *ptr = (char*)mmap(NULL, MAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 
     shm_unlink(path);
     _exit(0);
