472c2aff1fca902e2da667bde2100ee3333c8cdb
zhuichao001 <zhuichao001@163.com>
2023-11-19 10:40:09 +0800

    modify

--- ipc/mmap/mmp_stub.c
+++ ipc/mmap/mmp_stub.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
@@ -9,35 +8,43 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdio.h>
 
 #define FILE_MODE 0X766
 #define FILE_SIZE 8
 
-struct shared_t {
-    double val;
-    int count;
-}shared;
+int dev0_fd(){
+    int fd = open("/dev/zero", O_RDWR, 0);
+    return fd;
+}
+
+int file_fd(){
+    int fd = open("./zero", O_CREAT | O_RDWR, FILE_MODE);
+    return fd;
+}
+
+int shm_fd(){
+    const char *path = "tmp.shm_create";
+    int flags = O_RDWR | O_CREAT;
+    int fd = shm_open(path, flags, FILE_MODE);
+    return fd;
+}
 
 int main(int argc, char **argv){
-    shared.val = 3.1415926;
-    shared.count = 6;
-    int fd = open("/dev/zero", O_RDWR, 0);
-    //int fd = open("./zero", O_CREAT | O_RDWR, FILE_MODE);
+    int fd = file_fd();
     ftruncate(fd, FILE_SIZE);
-    //write(fd, &shared, sizeof(shared));
-    //close(fd);
     char *ptr = (char*)(struct shared_t *)mmap(NULL, FILE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
     memcpy(ptr, "hello..", 8);
 
     pid_t pid;
     if((pid=fork())==0){ /*child*/
         char fdstr[10];
-	memset(fdstr,0,10);
-	sprintf(fdstr,"%d", fd);
-	printf("before fd:%s\n", fdstr);
-	if(execlp("python", "python", "worker.py", fdstr, (char *)0)){
-		perror("failed");
-	}
+    memset(fdstr,0,10);
+    sprintf(fdstr,"%d", fd);
+    printf("before fd:%s\n", fdstr);
+    if(execlp("python", "python", "worker.py", fdstr, (char *)0)){
+        perror("failed");
+    }
         //exit(0);
     }else{ /*parent*/
         waitpid(pid, NULL, 0);
